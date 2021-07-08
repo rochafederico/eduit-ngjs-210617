aaf7c4b61740386a0ef1d6cdff2504661063751f
Federico Rocha Williams <federiconrocha@gmail.com>
2021-07-08 18:34:12 -0300

    Clase 5

--- modules/empleados.module.js
+++ modules/empleados.module.js
@@ -1,6 +1,6 @@
 angular
   .module("empleados", [])
-  .controller("empleadosController", function($scope) {
+  .controller("empleadosController", function($scope, $filter) {
     $scope.empleados = [
       { nombre: "Eze", apellido: "López", estaCoucheado: false },
       { nombre: "Gonza", apellido: "Garcia", estaCoucheado: true },
@@ -26,4 +26,38 @@ angular
         $scope.reverseOrden = !$scope.reverseOrden;
       }
     };
+    $scope.agregarEmpleado = function() {
+      var yaExisteEmpleado = $filter("yaExisteEmpleado");
+      if ($scope.nuevoEmpleado.$valid && !yaExisteEmpleado($scope.empelado, $scope.empleados)) {
+        $scope.empleados.push(angular.copy($scope.empelado));
+        $scope.empelado = {
+          nombre: "",
+          apellido: "",
+          estaCoucheado: false,
+        };
+      }
+    };
+    $scope.patronMail = "[a-z]{1,}[@][a-z]{1,}[.]{1,1}[a-z]{2,}";
+    $scope.cambioPais = function(){
+      if($scope.empelado.pais) {
+        $scope.provincias= ["Chubut", "Buenos Aires", "Cordoba", "Misiones"];
+      } else {
+        $scope.provincias= [];
+      }
+      $scope.empelado.provincia = '';
+      $scope.empelado.ciudad = '';
+    }
+
+    $scope.$watch('empelado.provincia', function(newValue, current) {
+      if(newValue) {
+          $scope.ciudades= ["Bagota", "Buenos Aires", "Caracas", "Puerto Madryn"];
+      }
+      $scope.empelado.ciudad = '';
+    })
+    $scope.$watchGroup(['empelado.provincia', 'empelado.ciudad'], function(newValue, current) {
+      console.log(newValue, current);
+    })
+    $scope.$watchCollection($scope.empleados, function(newValue, current) {
+      console.log(newValue, current);
+    })
   });
---
+++ modules/filters/ya-existe-empleado.filter.js
@@ -0,0 +1,16 @@
+angular.module("empleados").filter("yaExisteEmpleado", function() {
+  return function(empleadoActual, lista) {
+    var resultado = false;
+    for (let i = 0; i < lista.length; i++) {
+      const empleado = lista[i];
+      if (
+        empleado.nombre == empleadoActual.nombre &&
+        empleado.apellido == empleadoActual.apellido
+      ) {
+        resultado = true;
+        break;
+      }
+    }
+    return resultado;
+  };
+});
